6c191fd4bf9a8de2208870da939df5bcb099ff3f
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2022-09-24 10:14:28 -0300

    1

--- pwaweb/index.js
+++ pwaweb/index.js
@@ -30,9 +30,9 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
+        console.log('User accepted the Builder');
       } else {
-        console.log('User dismissed the A2HS prompt');
+        console.log('User dismissed the Builder');
       }
       deferredPrompt = null;
     });
