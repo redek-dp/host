1cb0a9b58f59c78df5320e3e7bf165613ba6709d
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2022-09-23 23:04:07 -0300

    1

--- pwaweb/sw.js
+++ pwaweb/sw.js
@@ -4,6 +4,10 @@ self.addEventListener('install', (e) => {
       '/host/pwaweb/',
       '/host/pwaweb/index.html',
       '/host/pwaweb/index.js',
+      'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css',
+      'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css',
+      'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js',
+      '/host/pwaweb/manifest.webmanifest',
     ])),
   );
 });
