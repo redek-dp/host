17d3404a85f4954c8d7058052ba0fce9d5b44d62
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2022-09-24 10:03:54 -0300

    1

--- pwaweb/sw.js
+++ pwaweb/sw.js
@@ -1,9 +1,14 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('fox-kop').then((cache) => cache.addAll([
+    caches.open('pwa-web').then((cache) => cache.addAll([
       '/host/pwaweb/',
       '/host/pwaweb/index.html',
       '/host/pwaweb/index.js',
+      '/host/pwaweb/icon/36.png',
+      '/host/pwaweb/icon/72.png',
+      '/host/pwaweb/icon/144.png',
+      '/host/pwaweb/icon/256.png',
+      '/host/pwaweb/icon/512.png',
       'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css',
       'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css',
       'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js',
