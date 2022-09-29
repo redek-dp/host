1f01917d7a36a7c01128e92840d334d03cc0fc0f
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2022-09-29 14:55:47 -0300

    1

---
+++ web/pxw.js
@@ -0,0 +1,32 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js', {
+        scope: './'
+    })
+        .then(function (registration) {
+            console.log('Service Worker Registered');
+        });
+    navigator.serviceWorker.ready.then(function (registration) {
+        console.log('Service Worker Ready');
+    });
+}
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', function (e) {
+    console.log('beforeinstallprompt Event fired');
+    //e.preventDefault();
+    deferredPrompt = e;
+    return false;
+});
+btnAddToHomeScreen.addEventListener('click', function () {
+    if (deferredPrompt !== undefined) {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then(function (choiceResult) {
+            console.log(choiceResult.outcome);
+            if (choiceResult.outcome == 'dismissed') {
+                console.log('User cancelled home screen install');
+            } else {
+                console.log('User added to home screen');
+            }
+            deferredPrompt = null;
+        });
+    }
+});
